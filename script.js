03c03caed7f15a314995c6e7512ffad91952726d
miriamsafwat2 <miriam.safwat@gmail.com>
2021-04-03 22:38:51 +0200

    feat: Change background

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+let verseContainer;
+
+document.addEventListener("DOMContentLoaded", function () {
+  verseContainer = document.querySelector(".verse-containter");
+  verseContainer.style.display = "none";
+});
+
 function generateVerse() {
+  verseContainer.style.display = "block";
   let randomVerse = getRandomVerse();
   let username = getUsername();
-  let personalVerse = randomVerse[0].replace("يوزر", username);
+  let personalVerse = randomVerse[0].replaceAll("يوزر", username);
 
   let txtVerse = document.getElementById("txtVerse");
-  txtVerse.innerHTML = '<span><i class="fas fa-quote-left"></i></span> ' +
-    personalVerse;
+  txtVerse.innerHTML =
+    '<span><i class="fas fa-quote-left"></i></span> ' + personalVerse;
 
   let txtShahed = document.getElementById("shahed");
   txtShahed.innerHTML = " ~" + randomVerse[1];
@@ -18,10 +26,4 @@ function getRandomVerse() {
 
 function getUsername() {
   return document.getElementById("txtName").value;
-}
-
-function tweet() {
-  let twitterLink = document.getElementsByClassName("twitter-share-button")[0];
-  let txtQuote = document.getElementById("txtQuote");
-  twitterLink.href += txtQuote.innerText;
 }
